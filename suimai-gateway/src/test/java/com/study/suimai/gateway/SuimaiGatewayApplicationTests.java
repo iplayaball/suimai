e75623749a5211f1162ec3c29b7cf5963aba091a
wangdajunttf <954354293@qq.com>
2021-07-14 18:12:48 +0800

    重新创建新的网关微服务suimai-gateway 启动注册成功； 测试网关路由到qq,baidu成功

---
+++ suimai-gateway/src/test/java/com/study/suimai/gateway/SuimaiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.suimai.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuimaiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
