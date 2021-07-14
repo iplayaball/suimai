f09acdde195d852d0ea5c314b33901977577144e
wangdajunttf <954354293@qq.com>
2021-07-14 12:58:07 +0800

    用spring boot test 测试MySQL的基本的crud

--- product/src/main/java/com/study/suimai/product/ProductApplication.java
+++ product/src/main/java/com/study/suimai/product/ProductApplication.java
@@ -1,8 +1,10 @@
 package com.study.suimai.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.study.suimai.product.dao")
 @SpringBootApplication
 public class ProductApplication {
 
--- product/src/test/java/com/study/suimai/product/ProductApplicationTests.java
+++ product/src/test/java/com/study/suimai/product/ProductApplicationTests.java
@@ -1,13 +1,32 @@
 package com.study.suimai.product;
 
+import com.study.suimai.product.entity.BrandEntity;
+import com.study.suimai.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ProductApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    BrandService brandService;
 
+    @Test
+    void contextLoads() {
+//		BrandEntity brandEntity = new BrandEntity();
+//		brandEntity.setName("华为");
+//		brandService.save(brandEntity);
+
+//		brandEntity.setBrandId(2L);
+//		brandEntity.setDescript("华为手机");
+//		brandService.updateById(brandEntity);
+
+        List<BrandEntity> list = brandService.list();
+        list.forEach((entity) -> {
+            System.out.println("entity = " + entity);
+        });
+    }
 }
