6f5969ff4afe8e625b3e30a79045520ff86b6941
wangdajunttf <954354293@qq.com>
2021-07-14 14:39:32 +0800

    生成其它服务的基本代码，测试服务注册到nacos失败

--- product/src/main/java/com/study/suimai/product/ProductApplication.java
+++ product/src/main/java/com/study/suimai/product/ProductApplication.java
@@ -3,7 +3,9 @@ package com.study.suimai.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.study.suimai.product.dao")
 @SpringBootApplication
 public class ProductApplication {
