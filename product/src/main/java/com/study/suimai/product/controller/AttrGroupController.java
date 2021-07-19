06641101a501add878525df73428072eb5c891e1
wangdajunttf <954354293@qq.com>
2021-07-19 16:40:01 +0800

    三级分类id和搜索关键字作为条件分页查询

--- product/src/main/java/com/study/suimai/product/controller/AttrGroupController.java
+++ product/src/main/java/com/study/suimai/product/controller/AttrGroupController.java
@@ -1,19 +1,14 @@
 package com.study.suimai.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.study.suimai.product.entity.AttrGroupEntity;
-import com.study.suimai.product.service.AttrGroupService;
 import com.study.common.utils.PageUtils;
 import com.study.common.utils.R;
+import com.study.suimai.product.entity.AttrGroupEntity;
+import com.study.suimai.product.service.AttrGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 
 
@@ -33,10 +28,11 @@ public class AttrGroupController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/{catelogId}")
     // @RequiresPermissions("product:attrgroup:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = attrGroupService.queryPage(params);
+    public R list(@RequestParam Map<String, Object> params,
+                  @PathVariable long catelogId){
+        PageUtils page = attrGroupService.queryPage(params, catelogId);
 
         return R.ok().put("page", page);
     }
--- product/src/main/java/com/study/suimai/product/service/impl/AttrGroupServiceImpl.java
+++ product/src/main/java/com/study/suimai/product/service/impl/AttrGroupServiceImpl.java
@@ -1,16 +1,17 @@
 package com.study.suimai.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.study.common.utils.PageUtils;
 import com.study.common.utils.Query;
-
 import com.study.suimai.product.dao.AttrGroupDao;
 import com.study.suimai.product.entity.AttrGroupEntity;
 import com.study.suimai.product.service.AttrGroupService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("attrGroupService")
@@ -21,6 +22,27 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         IPage<AttrGroupEntity> page = this.page(
                 new Query<AttrGroupEntity>().getPage(params),
                 new QueryWrapper<AttrGroupEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, long catelogId) {
+        LambdaQueryWrapper<AttrGroupEntity> wrapper = new LambdaQueryWrapper<>();
+        Object searchKey = params.get("key");
+        wrapper.eq(catelogId != 0, AttrGroupEntity::getCatelogId, catelogId)
+                // 搜索字符串等于id或者like分组名称
+                .and(searchKey != null,
+                        wrapperKey -> {
+                            wrapperKey
+                                    .eq(AttrGroupEntity::getAttrGroupId, searchKey)
+                                    .or()
+                                    .like(AttrGroupEntity::getAttrGroupName, searchKey);
+                });
+        IPage<AttrGroupEntity> page = this.page(
+                new Query<AttrGroupEntity>().getPage(params),
+                wrapper
         );
 
         return new PageUtils(page);
