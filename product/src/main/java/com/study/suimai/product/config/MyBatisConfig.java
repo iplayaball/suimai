1f88f87696bbb9e1a5ec64fcbab81ca0b48908ff
wangdajunttf <954354293@qq.com>
2021-07-19 19:12:08 +0800

    前端正常显示分页信息，总页数

---
+++ product/src/main/java/com/study/suimai/product/config/MyBatisConfig.java
@@ -0,0 +1,24 @@
+package com.study.suimai.product.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.study.suimai.product.dao")
+public class MyBatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+//        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+//        paginationInterceptor.setOverflow(true);
+//        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+//        paginationInterceptor.setLimit(1000);
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+}
