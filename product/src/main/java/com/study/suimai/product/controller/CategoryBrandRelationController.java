1dc77ddc393d44021e6682cda652e52168d7c958
wangdajunttf <954354293@qq.com>
2021-07-19 19:50:44 +0800

    品牌列表根据ID或者名称搜索

--- product/src/main/java/com/study/suimai/product/controller/CategoryBrandRelationController.java
+++ product/src/main/java/com/study/suimai/product/controller/CategoryBrandRelationController.java
@@ -1,19 +1,14 @@
 package com.study.suimai.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.study.suimai.product.entity.CategoryBrandRelationEntity;
-import com.study.suimai.product.service.CategoryBrandRelationService;
 import com.study.common.utils.PageUtils;
 import com.study.common.utils.R;
+import com.study.suimai.product.entity.CategoryBrandRelationEntity;
+import com.study.suimai.product.service.CategoryBrandRelationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 
 
@@ -36,6 +31,17 @@ public class CategoryBrandRelationController {
     @RequestMapping("/list")
     // @RequiresPermissions("product:categorybrandrelation:list")
     public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = categoryBrandRelationService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/catelog/list")
+    // @RequiresPermissions("product:categorybrandrelation:list")
+    public R listByBrandId(@RequestParam Map<String, Object> params){
         PageUtils page = categoryBrandRelationService.queryPage(params);
 
         return R.ok().put("page", page);
--- product/src/main/java/com/study/suimai/product/service/impl/BrandServiceImpl.java
+++ product/src/main/java/com/study/suimai/product/service/impl/BrandServiceImpl.java
@@ -1,16 +1,17 @@
 package com.study.suimai.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mysql.cj.util.StringUtils;
 import com.study.common.utils.PageUtils;
 import com.study.common.utils.Query;
-
 import com.study.suimai.product.dao.BrandDao;
 import com.study.suimai.product.entity.BrandEntity;
 import com.study.suimai.product.service.BrandService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("brandService")
@@ -18,9 +19,21 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        String searchKey = (String) params.get("key");
+
+        LambdaQueryWrapper<BrandEntity> wrapper = new LambdaQueryWrapper<>();
+        wrapper.and(! StringUtils.isNullOrEmpty(searchKey),
+                wrapper1 -> {
+                wrapper1
+                    .eq(BrandEntity::getBrandId, searchKey)
+                    .or()
+                    .like(BrandEntity::getName, searchKey);
+        });
+
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
