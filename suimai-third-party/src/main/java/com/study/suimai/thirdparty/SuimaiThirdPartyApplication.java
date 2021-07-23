253d331d20549c974ce5b34b1dca1f0dec268288
wangdajunttf <954354293@qq.com>
2021-07-23 17:39:14 +0800

    新增第三方服务，调用 oss 服务，测试通过

---
+++ suimai-third-party/src/main/java/com/study/suimai/thirdparty/SuimaiThirdPartyApplication.java
@@ -0,0 +1,13 @@
+package com.study.suimai.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuimaiThirdPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuimaiThirdPartyApplication.class, args);
+	}
+
+}
