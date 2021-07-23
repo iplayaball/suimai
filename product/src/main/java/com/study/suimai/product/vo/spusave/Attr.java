8ae34fd8f83ed81f6e178f59950d39ef9f7774c7
wangdajunttf <954354293@qq.com>
2021-07-23 14:25:09 +0800

    新增商品，导入json生成的java bean

---
+++ product/src/main/java/com/study/suimai/product/vo/spusave/Attr.java
@@ -0,0 +1,20 @@
+/**
+  * Copyright 2019 bejson.com 
+  */
+package com.study.suimai.product.vo.spusave;
+
+import lombok.Data;
+
+/**
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+@Data
+public class Attr {
+
+    private Long attrId;
+    private String attrName;
+    private String attrValue;
+
+}
---
+++ product/src/main/java/com/study/suimai/product/vo/spusave/Bounds.java
@@ -0,0 +1,22 @@
+/**
+  * Copyright 2019 bejson.com 
+  */
+package com.study.suimai.product.vo.spusave;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+@Data
+public class Bounds {
+
+    private BigDecimal buyBounds;
+    private BigDecimal growBounds;
+
+
+}
