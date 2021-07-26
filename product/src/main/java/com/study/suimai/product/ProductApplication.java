969e597b57cabfd3dcb70833d2c3ff9ef0f6df82
wangdajunttf <954354293@qq.com>
2021-07-26 14:26:55 +0800

    首页渲染的二级三级分类数据，使用redis缓存

--- product/src/main/java/com/study/suimai/product/ProductApplication.java
+++ product/src/main/java/com/study/suimai/product/ProductApplication.java
@@ -2,8 +2,10 @@ package com.study.suimai.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCaching
 @EnableFeignClients(basePackages = "com.study.suimai.product.feign")
 //@EnableDiscoveryClient
 //@MapperScan("com.study.suimai.product.dao")
