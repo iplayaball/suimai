e262ef329d1c8aa3d3b32288cd463fc5373fa285
wangdajunttf <954354293@qq.com>
2021-07-27 20:54:14 +0800

    search 服务 @EnableFeignClients

--- suimai-search/src/main/java/com/study/suimai/search/SuimaiSearchApplication.java
+++ suimai-search/src/main/java/com/study/suimai/search/SuimaiSearchApplication.java
@@ -2,7 +2,9 @@ package com.study.suimai.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // 必须加，不然会ProductFeignService找不到这个 Bean
 @SpringBootApplication
 public class SuimaiSearchApplication {
 
