09f91c450fccfc457f4748ddb008b52c4c34df1a
wangdajunttf <954354293@qq.com>
2021-07-29 09:54:46 +0800

    认证服务 验证码防刷校验

--- suimai-auth-server/src/main/java/com/study/suimai/auth/controller/LoginController.java
+++ suimai-auth-server/src/main/java/com/study/suimai/auth/controller/LoginController.java
@@ -1,11 +1,17 @@
 package com.study.suimai.auth.controller;
 
+import com.study.common.constant.AuthServerConstant;
+import com.study.common.exception.BizCodeEnum;
 import com.study.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.TimeUnit;
 
 
 @Controller
@@ -14,47 +20,51 @@ public class LoginController {
 //    @Autowired
 //    private ThirdPartFeignService thirdPartFeignService;
 
+  @Autowired
+  private StringRedisTemplate stringRedisTemplate;
 
-    @ResponseBody
-    @GetMapping(value = "/sms/sendCode")
-    public R sendCode(@RequestParam("phone") String phone) {
 
-        /*//1、接口防刷
-        String redisCode = stringRedisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone);
-        if (!StringUtils.isEmpty(redisCode)) {
-            //活动存入redis的时间，用当前时间减去存入redis的时间，判断用户手机号是否在60s内发送验证码
-            long currentTime = Long.parseLong(redisCode.split("_")[1]);
-            if (System.currentTimeMillis() - currentTime < 60000) {
-                //60s内不能再发
-                return R.error(BizCodeEnum.SMS_CODE_EXCEPTION.getCode(),BizCodeEnum.SMS_CODE_EXCEPTION.getMessage());
-            }
-        }
+  @ResponseBody
+  @GetMapping(value = "/sms/sendCode")
+  public R sendCode(@RequestParam("phone") String phone) {
 
-        //2、验证码的再次效验 redis.存key-phone,value-code
-        int code = (int) ((Math.random() * 9 + 1) * 100000);
-        String codeNum = String.valueOf(code);
-        String redisStorage = codeNum + "_" + System.currentTimeMillis();
-
-        //存入redis，防止同一个手机号在60秒内再次发送验证码
-        stringRedisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX+phone,
-                redisStorage,10, TimeUnit.MINUTES);
-
-        thirdPartFeignService.sendCode(phone, codeNum);*/
-        System.out.println("发送验证码测试");
-
-        return R.ok();
+    //1、接口防刷
+    String redisCode = stringRedisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone);
+    if (!StringUtils.isEmpty(redisCode)) {
+      //活动存入redis的时间，用当前时间减去存入redis的时间，判断用户手机号是否在60s内发送验证码
+      long currentTime = Long.parseLong(redisCode.split("_")[1]);
+      if (System.currentTimeMillis() - currentTime < 60000) {
+        //60s内不能再发
+        return R.error(BizCodeEnum.SMS_CODE_EXCEPTION.getCode(), BizCodeEnum.SMS_CODE_EXCEPTION.getMessage());
+      }
     }
 
+    //2、验证码的再次效验 redis.存key-phone,value-code
+    int code = (int) ((Math.random() * 9 + 1) * 100000);
+    String codeNum = String.valueOf(code);
+    String redisStorage = codeNum + "_" + System.currentTimeMillis();
 
-    /**
-     *
-     * TODO: 重定向携带数据：利用session原理，将数据放在session中。
-     * TODO:只要跳转到下一个页面取出这个数据以后，session里面的数据就会删掉
-     * TODO：分布下session问题
-     * RedirectAttributes：重定向也可以保留数据，不会丢失
-     * 用户注册
-     * @return
-     */
+    //存入redis，防止同一个手机号在60秒内再次发送验证码
+    stringRedisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone,
+     redisStorage, 10, TimeUnit.MINUTES);
+
+//        thirdPartFeignService.sendCode(phone, codeNum);
+    System.out.printf("%s:%s", phone, code);
+    System.out.println();
+
+    return R.ok();
+  }
+
+
+  /**
+   *
+   * TODO: 重定向携带数据：利用session原理，将数据放在session中。
+   * TODO:只要跳转到下一个页面取出这个数据以后，session里面的数据就会删掉
+   * TODO：分布下session问题
+   * RedirectAttributes：重定向也可以保留数据，不会丢失
+   * 用户注册
+   * @return
+   */
     /*@PostMapping(value = "/register")
     public String register(@Valid UserRegisterVo vos, BindingResult result,
                            RedirectAttributes attributes) {
