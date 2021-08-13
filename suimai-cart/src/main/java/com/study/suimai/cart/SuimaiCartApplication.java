f042d4a8d47b36eda302dd38698289edbd9d1fda
wangdajunttf <954354293@qq.com>
2021-08-13 16:13:13 +0800

    创建购物车微服务项目

---
+++ suimai-cart/src/main/java/com/study/suimai/cart/SuimaiCartApplication.java
@@ -0,0 +1,13 @@
+package com.study.suimai.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuimaiCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuimaiCartApplication.class, args);
+	}
+
+}
