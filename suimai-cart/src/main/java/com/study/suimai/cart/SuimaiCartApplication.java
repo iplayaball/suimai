daf92f87c71bf6cfeabb50a954fcdf21ec12b9d1
wangdajunttf <954354293@qq.com>
2021-08-13 18:16:03 +0800

    购物车配置一些基础配置

--- suimai-cart/src/main/java/com/study/suimai/cart/SuimaiCartApplication.java
+++ suimai-cart/src/main/java/com/study/suimai/cart/SuimaiCartApplication.java
@@ -2,7 +2,9 @@ package com.study.suimai.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SuimaiCartApplication {
 
